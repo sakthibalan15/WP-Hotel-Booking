aac2f52e8dd3ad6da669a2654674a2383fc28064
sakthibalan <bala.psd@gmail.com>
2018-04-12 13:25:28 +0530

    arrival date to from date search page

--- templates/search/search.php
+++ templates/search/search.php
@@ -19,21 +19,21 @@ $uniqid         = uniqid();
 		$atts = $args['atts'];
 	if ( !isset( $atts['show_title'] ) || strtolower( $atts['show_title'] ) === 'true' ):
 		?>
-        <h3><?php _e( 'Search your e-cycle', 'wp-hotel-booking' ); ?></h3>
+        <h3><?php _e( 'Book your e-cycle', 'wp-hotel-booking' ); ?></h3>
 	<?php endif; ?>
     <form name="hb-search-form" action="<?php echo esc_attr( $search_page ); ?>" class="hb-search-form-<?php echo esc_attr( $uniqid ) ?>">
         <ul class="hb-form-table">
             <li class="hb-form-field">
-				<?php hb_render_label_shortcode( $atts, 'show_label', __( 'Arrival Date', 'wp-hotel-booking' ), 'true' ); ?>
+				<?php hb_render_label_shortcode( $atts, 'show_label', __( 'From Date', 'wp-hotel-booking' ), 'true' ); ?>
                 <div class="hb-form-field-input hb_input_field">
-                    <input type="text" name="check_in_date" id="check_in_date_<?php echo esc_attr( $uniqid ); ?>" class="hb_input_date_check" value="<?php echo esc_attr( $check_in_date ); ?>" placeholder="<?php _e( 'Arrival Date', 'wp-hotel-booking' ); ?>" />
+                    <input type="text" name="check_in_date" id="check_in_date_<?php echo esc_attr( $uniqid ); ?>" class="hb_input_date_check" value="<?php echo esc_attr( $check_in_date ); ?>" placeholder="<?php _e( 'From Date', 'wp-hotel-booking' ); ?>" />
                 </div>
             </li>
 
             <li class="hb-form-field">
-				<?php hb_render_label_shortcode( $atts, 'show_label', __( 'Departure Date', 'wp-hotel-booking' ), 'true' ); ?>
+				<?php hb_render_label_shortcode( $atts, 'show_label', __( 'To Date', 'wp-hotel-booking' ), 'true' ); ?>
                 <div class="hb-form-field-input hb_input_field">
-                    <input type="text" name="check_out_date" id="check_out_date_<?php echo esc_attr( $uniqid ) ?>" class="hb_input_date_check" value="<?php echo esc_attr( $check_out_date ); ?>" placeholder="<?php _e( 'Departure Date', 'wp-hotel-booking' ); ?>" />
+                    <input type="text" name="check_out_date" id="check_out_date_<?php echo esc_attr( $uniqid ) ?>" class="hb_input_date_check" value="<?php echo esc_attr( $check_out_date ); ?>" placeholder="<?php _e( 'To Date', 'wp-hotel-booking' ); ?>" />
                 </div>
             </li>
 
